2f9a81a9b0396a2d116dff08a6ea5185f28de1b6
Neo <neo.lee@kakao.com>
2020-09-27 20:25:37 +0900

    SNS added

--- src/Upper.js
+++ src/Upper.js
@@ -4,12 +4,17 @@ import "./Upper.css";
 function Upper(){
     return <div className="upper_container">
 
-        <div className="profile_img">
-           <img src="https://github.com/neo-seokhee/profile_site/blob/master/src/profile3.png?raw=true" />
+        <div>
+           <img className="profile_img" src="https://github.com/neo-seokhee/profile_site/blob/master/src/profile3.png?raw=true" />
         </div>
 
         <h1 className="upper_title">Hello I'm<br/><span className="title_neo">NEO</span></h1>
         <h3 className="upper_des">안녕하세요! 이석희입니다.<br/>밸런스히어로에서 <span className="highlight">Product Manager</span>로 일하고 있구요.<br/>새로운 것을 배우는 걸 좋아합니다.</h3>
+
+    <div className="social">
+        <a href="https://brunch.co.kr/@neolee91"><img src="https://junwoolee.me/static/media/BrunchBG.cba32d26.png" /></a>
+        <a href="https://www.instagram.com/neo_seokhee/"><img src="https://junwoolee.me/static/media/InstaBG.31a77afd.png" /></a>              
+    </div>
 
         <h3 className="timeline">Timeline</h3>
     </div>
