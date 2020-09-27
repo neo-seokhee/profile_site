77412bc5e28235628943aa73387186f5818b390a
NEO <neo@NEOui-MacBookPro.local>
2020-09-27 18:59:28 +0900

    Img Changed

--- src/Upper.js
+++ src/Upper.js
@@ -4,7 +4,9 @@ import "./Upper.css";
 function Upper(){
     return <div className="upper_container">
 
-        <img src="" />
+        <div className="profile_img">
+           <img src="https://github.com/neo-seokhee/profie_site/blob/master/src/profile2.png?raw=true" />
+        </div>
 
         <h1 className="upper_title">Hello I'm<br/><span className="title_neo">NEO</span></h1>
         <h3 className="upper_des">안녕하세요! 이석희입니다.<br/>밸런스히어로에서 <span className="highlight">Product Manager</span>로 일하고 있구요.<br/>새로운 것을 배우는 걸 좋아합니다.</h3>
